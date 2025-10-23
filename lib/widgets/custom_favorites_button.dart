7f2f52c5f045ebf27d8ab45427d0f64b291ed221
Your Name <you@example.com>
2025-10-23 20:19:57 +0300

    add favorite option in comic view

--- lib/widgets/custom_favorites_button.dart
+++ lib/widgets/custom_favorites_button.dart
@@ -1,10 +1,63 @@
+import 'package:comic_knight/const.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class CustomFavoritesButton extends StatelessWidget {
+
+class CustomFavoritesButton extends StatefulWidget {
   const CustomFavoritesButton({required this.id});
   final int id;
+
+  @override
+  State<CustomFavoritesButton> createState() => _CustomFavoritesButtonState();
+}
+
+class _CustomFavoritesButtonState extends State<CustomFavoritesButton> {
+    final supabase = Supabase.instance.client;
+    String  chick='null';
+chickFavorite()async{
+  final  response = await supabase
+  .from('favorite')
+  .select().eq('user', kUser!.email.toString()).eq('id_comic', widget.id).maybeSingle();
+  if(response==null){
+    chick='false';
+  }else{
+    chick='true';
+  }
+  if (mounted) {
+  setState(() {
+    
+  });
+}
+}
+
+  @override
+  void initState() {
+    super.initState();
+    chickFavorite();
+  }
   @override
   Widget build(BuildContext context) {
-    return IconButton(onPressed: (){}, icon: Icon(Icons.favorite_outline));
+    return  chick=='null'?IconButton(icon: Icon(Icons.favorite_outline),onPressed: () {
+      
+    },): IconButton(onPressed: ()async{
+      if (chick=='true') {
+        chick='false';
+  await supabase
+    .from('favorite')
+    .delete()
+    .eq('id_comic', widget.id).eq('user', kUser!.email.toString());
+    setState(() {
+      
+    });
+}else if(chick=='false'){
+  chick='true';
+  await supabase
+    .from('favorite')
+    .insert({'id_comic': widget.id, 'user': kUser!.email.toString()});
+    setState(() {
+      
+    });
+}
+    }, icon:chick=='false'? Icon(Icons.favorite_outline):Icon(Icons.favorite));
   }
 }
